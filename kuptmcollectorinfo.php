b4417e7f0705602dbb016d52769a15fed6b27cf0
Dxhxm88 <ahmadaham99@gmail.com>
2019-12-15 18:37:41 +0800

    Change title

--- kuptmcollectorinfo.php
+++ kuptmcollectorinfo.php
@@ -8,7 +8,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>KUPTM Courier Management System</title>
+<title>KUPTM Courier Management Systems</title>
 <link href="css/style.css" rel="stylesheet" type="text/css" />
 </head>
 
